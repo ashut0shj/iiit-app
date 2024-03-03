f9751e3ed1925faf1c725debf9a8dca085218d8e
ashut0sh <ashutoshj2810@gmail.com>
2024-03-03 23:52:40 +0530

    app

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/article_model.dart';
 import '../widgets/custom_tag.dart';
-import '../widgets/image_container.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
 
   static const routeName = '/article';
+
   @override
   Widget build(BuildContext context) {
     final article = ModalRoute.of(context)!.settings.arguments as Article;
-    return ImageContainer(
-      width: double.infinity,
-      imageUrl: article.imageUrl,
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          iconTheme: const IconThemeData(color: Colors.white),
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        extendBodyBehindAppBar: true,
-        body: ListView(
-          children: [
-            _NewsHeadline(article: article),
-            _NewsBody(article: article)
-          ],
-        ),
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final appBarHeight = AppBar().preferredSize.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    final imageHeight = screenHeight - appBarHeight - statusBarHeight;
+    final imageWidth = screenWidth;
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: imageHeight,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                article.imageUrl,
+                width: imageWidth,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: _NewsBody(article: article),
+          ),
+        ],
       ),
     );
   }
@@ -46,14 +54,8 @@ class _NewsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20.0),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-        ),
-        color: Colors.white,
-      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
@@ -69,9 +71,7 @@ class _NewsBody extends StatelessWidget {
                   const SizedBox(width: 10),
                   Text(
                     article.author,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Colors.white,
-                        ),
+                    style: TextStyle(color: Colors.white),
                   ),
                 ],
               ),
@@ -86,22 +86,6 @@ class _NewsBody extends StatelessWidget {
                   const SizedBox(width: 10),
                   Text(
                     '${DateTime.now().difference(article.createdAt).inHours}h',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                ],
-              ),
-              const SizedBox(width: 10),
-              CustomTag(
-                backgroundColor: Colors.grey.shade200,
-                children: [
-                  const Icon(
-                    Icons.remove_red_eye,
-                    color: Colors.grey,
-                  ),
-                  const SizedBox(width: 10),
-                  Text(
-                    '${article.views}',
-                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ],
               ),
@@ -110,16 +94,16 @@ class _NewsBody extends StatelessWidget {
           const SizedBox(height: 20),
           Text(
             article.title,
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .copyWith(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            article.subtitle,
+            style: TextStyle(color: Colors.black54),
           ),
           const SizedBox(height: 20),
           Text(
             article.body,
-            style:
-                Theme.of(context).textTheme.bodyMedium!.copyWith(height: 1.5),
+            style: TextStyle(height: 1.5),
           ),
           const SizedBox(height: 20),
           Row(
@@ -127,70 +111,15 @@ class _NewsBody extends StatelessWidget {
             children: [
               ElevatedButton(
                 onPressed: () => launchUrl(Uri.parse('https://docs.google.com/forms/d/1RD3cAPM7HWpZo622atupB099lUpij5QF9bvBr5vRfmA/viewform?edit_requested=true')),
-                child: Text('Register'),
+                child: const Text('Register'),
               ),
               ElevatedButton(
                 onPressed: () {
                   // Handle add to calendar button press
                 },
-                child: Text('Add to Calendar'),
+                child: const Text('Add to Calendar'),
               ),
             ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-class _NewsHeadline extends StatelessWidget {
-  const _NewsHeadline({
-    Key? key,
-    required this.article,
-  }) : super(key: key);
-
-  final Article article;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.15,
-          ),
-          CustomTag(
-            backgroundColor: Colors.grey.withAlpha(150),
-            children: [
-              Text(
-                article.category,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Colors.white,
-                    ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Text(
-            article.title,
-            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  height: 1.25,
-                ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            article.subtitle,
-            style: Theme.of(context)
-                .textTheme
-                .bodyMedium!
-                .copyWith(color: Colors.white),
           ),
         ],
       ),
