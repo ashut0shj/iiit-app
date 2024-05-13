30ff9d7e580165ddbc1d99ca9fe6557a2e052162
kanika1-13 <kanika22102@iiitnr.edu.in>
2024-05-13 15:22:57 +0530

    forms final part3

---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:iiitnr/screens/screens.dart';
+import 'package:iiitnr/testf.dart';
+import 'package:iiitnr/screens/club.dart';
+import 'package:iiitnr/screens/forms.dart';
+import 'package:iiitnr/screens/discover.dart';
+
+
+class MyAppDrawer extends StatelessWidget {
+  const MyAppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const UserAccountsDrawerHeader(
+            accountName: Text('username'),
+            accountEmail: Text('user@iiit.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Icon(
+                  Icons.person_outline, // Using Icon widget instead of IconData directly
+                  size: 90,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                    'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg'),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.rss_feed),
+            title: const Text('Feed'),
+            onTap: () {Navigator.pushNamed(context, DiscoverScreen.routeName);},
+          ),
+          ListTile(
+            leading: const Icon(Icons.explore),
+            title: const Text('Explore'),
+            onTap: () {
+              Navigator.pushNamed(context, ExploreScreen.routeName);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.pie_chart),
+            title: const Text('My Events'),
+            onTap: () {Navigator.pushNamed(context, DiscoverScreen.routeName);},
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_circle),
+            title: const Text('User'),
+            onTap: () {Navigator.pushNamed(context, AdminScreen.routeName);},
+          ),
+    
+          ListTile(
+            leading: const Icon(Icons.event),
+            title: const Text('Forms'), // Add the option for Event Form
+            onTap: () {
+              Navigator.pushNamed(context, FormsScreen.routeName);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const TestForm()));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
